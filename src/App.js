c63b19419a019392763dd46cc770c49641653790
Ady Juliyanto <adyjuliyanto2001@gmail.com>
2021-04-19 22:51:16 +0700

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,412 @@
-import logo from './logo.svg';
+import React from 'react';
+// import logo from './logo.svg';
+import Portfolio from './components/Portfolio'
 import './App.css';
+import ks from '../src/assets/img/22.jpg';
+import ws from '../src/assets/img/qq.jpg';
+import kt from '../src/assets/img/dd.jpg';
 
 function App() {
+  const portfolioLinks = [
+    {
+      title: 'Pramuka',
+      caption: 'Gerakan Pramuka Indonesia adalah nama organisasi pendidikan nonformal yang menyelenggarakan pendidikan kepanduan di Indonesia',
+      image : 'https://cdn0-production-images-kly.akamaized.net/WqKF3lKc_B7Ad5KE-Sp7C4dxO4Y=/640x360/smart/filters:quality(75):strip_icc():format(jpeg)/kly-media-production/medias/2329031/original/089900800_1534214958-087792400_1471226502-Sejarah-Singkat-Kepramukaan-di-Indonesia.jpg'
+    },
+    {
+      title: 'Palang Merah Remaja',
+      caption: 'Palang Merah Remaja adalah wadah pembinaan dan pengembangan anggota remaja PMI, yang selanjutnya disebut PMR.',
+      image : 'https://beritabaik.id/static/upload/content/images/20200630/1593484174993-PMR.jpg'
+    },
+    {
+      title: 'Osis',
+      caption: ' suatu organisasi yang berada di tingkat sekolah di Indonesia yang dimulai dari Sekolah Menengah yaitu Sekolah Menengah Pertama (SMP)',
+      image : 'https://yru.or.id/wp-content/uploads/2017/12/pelantikan-osis-smp.jpg'
+    },
+    {
+      title: 'Pencak Silat',
+      caption: 'suatu seni bela diri tradisional yang berasal dari Kepulauan Nusantara. Seni bela diri ini secara luas dikenal di Indonesia',
+      image : 'http://www.smphangtuah3.info/wp-content/uploads/2020/03/Ekskur-Pencak-Silat-Siswa-SMP-Hang-Tuah-3-02.jpg'
+    },
+    {
+      title: 'Karate',
+      caption: 'seni bela diri yang berasal dari Jepang. Seni bela diri ini sedikit dipengaruhi oleh Seni bela diri Cina kenpō.',
+      image : 'https://www.projustisianews.com/bank_gambar/sedeng_21img-20190313-wa0023.jpg'
+    },
+    {
+      title: 'Karawitan',
+      caption: 'seni bela diri yang berasal dari Jepang. Seni bela diri ini sedikit dipengaruhi oleh Seni bela diri Cina kenpō.',
+      image : 'http://www.smpn4kepanjen.sch.id/wp-content/uploads/2018/01/eks-karawitan.jpg'
+    }    
+  ]
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav className="navbar navbar-expand-lg navbar-dark fixed-top" id="mainNav">
+    <div className="container">
+     
+      <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        Menu
+        <i className="fa fa-bars"></i>
+      </button>
+      <div className="collapse navbar-collapse" id="navbarResponsive">
+        <ul className="navbar-nav text-uppercase ml-auto">
+          <li className="nav-item">
+            <a className="nav-link js-scroll-trigger" href="#services">Pendaftaran</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link js-scroll-trigger" href="#portfolio">Ektrkulikuler</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link js-scroll-trigger" href="#about">Tentang</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link js-scroll-trigger" href="#team">Jajaran</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link js-scroll-trigger" href="#contact">Contact</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+  
+  <header className="masthead">
+    <div className="container">
+      <div className="intro-text">
+        <div className="intro-lead-in">Selamat Datang Di SMPN 1 Ngawi !</div>
+        <div className="intro-heading text-uppercase">Come and Join Us</div>
+        <a className="btn btn-primary btn-xl text-uppercase js-scroll-trigger" href="#services">Info Selengkapnya</a>
+      </div>
+    </div>
+  </header>
+
+  
+  <section className="page-section" id="services">
+    <div className="container">
+      <div className="row">
+        <div className="col-lg-12 text-center">
+          <h2 className="section-heading text-uppercase">Pendaftaran</h2>
+          <h3 className="section-subheading text-muted">Lorem ipsum dolor sit amet consectetur.</h3>
+        </div>
+      </div>
+      <div className="row text-center">
+        <div className="col-md-4">
+          <span className="fa-stack fa-4x">
+            <i className="fa fa-circle fa-stack-2x text-primary"></i>
+            <i className="fa fa-shopping-cart fa-stack-1x fa-inverse"></i>
+          </span>
+          <h4 className="service-heading">Verifikasi Email</h4>
+          <p className="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.</p>
+        </div>
+        <div className="col-md-4">
+          <span className="fa-stack fa-4x">
+            <i className="fa fa-circle fa-stack-2x text-primary"></i>
+            <i className="fa fa-laptop fa-stack-1x fa-inverse"></i>
+          </span>
+          <h4 className="service-heading">Isi Biodata</h4>
+          <p className="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.</p>
+        </div>
+        <div className="col-md-4">
+          <span className="fa-stack fa-4x">
+            <i className="fa fa-circle fa-stack-2x text-primary"></i>
+            <i className="fa fa-lock fa-stack-1x fa-inverse"></i>
+          </span>
+          <h4 className="service-heading">Daftar Ulang</h4>
+          <p className="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.</p>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  
+  <Portfolio portfolioLinks={portfolioLinks}></Portfolio>
+
+  
+  <section className="page-section" id="about">
+    <div className="container">
+      <div className="row">
+        <div className="col-lg-12 text-center">
+          <h2 className="section-heading text-uppercase">Tentang Kami</h2>
+          <h3 className="section-subheading text-muted">Lorem ipsum dolor sit amet consectetur.</h3>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-lg-12">
+          <ul className="timeline">
+            <li>
+              <div className="timeline-image">
+                <img className="rounded-circle img-fluid" src="img/about/1.jpg" alt=""/>
+              </div>
+              <div className="timeline-panel">
+                <div className="timeline-heading">
+                  <h4>2009-2011</h4>
+                  <h4 className="subheading">Sekolah Pertama Kali berdiri</h4>
+                </div>
+                <div className="timeline-body">
+                  <p className="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt ut voluptatum eius sapiente, totam reiciendis temporibus qui quibusdam, recusandae sit vero unde, sed, incidunt et ea quo dolore laudantium consectetur!</p>
+                </div>
+              </div>
+            </li>
+            <li className="timeline-inverted">
+              <div className="timeline-image">
+                <img className="rounded-circle img-fluid" src="img/about/2.jpg" alt=""/>
+              </div>
+              <div className="timeline-panel">
+                <div className="timeline-heading">
+                  <h4>March 2011</h4>
+                  <h4 className="subheading">Berkembang Dengan Cepat</h4>
+                </div>
+                <div className="timeline-body">
+                  <p className="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt ut voluptatum eius sapiente, totam reiciendis temporibus qui quibusdam, recusandae sit vero unde, sed, incidunt et ea quo dolore laudantium consectetur!</p>
+                </div>
+              </div>
+            </li>
+            <li>
+              <div className="timeline-image">
+                <img className="rounded-circle img-fluid" src="img/about/3.jpg" alt=""/>
+              </div>
+              <div className="timeline-panel">
+                <div className="timeline-heading">
+                  <h4>December 2012</h4>
+                  <h4 className="subheading">Banyak Prestasi Yang diraih</h4>
+                </div>
+                <div className="timeline-body">
+                  <p className="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt ut voluptatum eius sapiente, totam reiciendis temporibus qui quibusdam, recusandae sit vero unde, sed, incidunt et ea quo dolore laudantium consectetur!</p>
+                </div>
+              </div>
+            </li>
+            <li className="timeline-inverted">
+              <div className="timeline-image">
+                <img className="rounded-circle img-fluid" src="img/about/4.jpg" alt=""/>
+              </div>
+              <div className="timeline-panel">
+                <div className="timeline-heading">
+                  <h4>July 2014</h4>
+                  <h4 className="subheading">Dalam Sejarah Sekolah</h4>
+                </div>
+                <div className="timeline-body">
+                  <p className="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt ut voluptatum eius sapiente, totam reiciendis temporibus qui quibusdam, recusandae sit vero unde, sed, incidunt et ea quo dolore laudantium consectetur!</p>
+                </div>
+              </div>
+            </li>
+            <li className="timeline-inverted">
+              <div className="timeline-image">
+                <h4>Jadilah
+                  <br/>Bagian Dari
+                  <br/>Cerita Kami!</h4>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  
+  <section className="bg-light page-section" id="team">
+    <div className="container">
+      <div className="row">
+        <div className="col-lg-12 text-center">
+          <h2 className="section-heading text-uppercase">Jajaran</h2>
+          <h3 className="section-subheading text-muted">Lorem ipsum dolor sit amet consectetur.</h3>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-sm-4">
+          <div className="team-member">
+            <img className="mx-auto rounded-circle" src={ks} alt=""/>
+            <h4>Ady Juliyanto</h4>
+            <p className="text-muted">Kepala Sekolah</p>
+            <ul className="list-inline social-buttons">
+              <li className="list-inline-item">
+                <a href="#something">
+                  <i className="fa fa-twitter"></i>
+                </a>
+              </li>
+              <li className="list-inline-item">
+                <a href="#something">
+                  <i className="fa fa-facebook-f"></i>
+                </a>
+              </li>
+              <li className="list-inline-item">
+                <a href="#something">
+                  <i className="fa fa-linkedin-in"></i>
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="col-sm-4">
+          <div className="team-member">
+            <img className="mx-auto rounded-circle" src={ws} alt=""/>
+            <h4>Dimas Aji Pratama</h4>
+            <p className="text-muted">Wakil Kepala Sekolah</p>
+            <ul className="list-inline social-buttons">
+              <li className="list-inline-item">
+                <a href="#something">
+                  <i className="fa fa-twitter"></i>
+                </a>
+              </li>
+              <li className="list-inline-item">
+                <a href="#something">
+                  <i className="fa fa-facebook-f"></i>
+                </a>
+              </li>
+              <li className="list-inline-item">
+                <a href="#something">
+                  <i className="fa fa-linkedin-in"></i>
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="col-sm-4">
+          <div className="team-member">
+            <img className="mx-auto rounded-circle" src={kt} alt=""/>
+            <h4>Diana Setiawan</h4>
+            <p className="text-muted">Komite Sekolah</p>
+            <ul className="list-inline social-buttons">
+              <li className="list-inline-item">
+                <a href="#something">
+                  <i className="fa fa-twitter"></i>
+                </a>
+              </li>
+              <li className="list-inline-item">
+                <a href="#something">
+                  <i className="fa fa-facebook-f"></i>
+                </a>
+              </li>
+              <li className="list-inline-item">
+                <a href="#something">
+                  <i className="fa fa-linkedin-in"></i>
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-lg-8 mx-auto text-center">
+          <p className="large text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut eaque, laboriosam veritatis, quos non quis ad perspiciatis, totam corporis ea, alias ut unde.</p>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  
+  <section className="py-5">
+    <div className="container">
+      <div className="row">
+        <div className="col-md-3 col-sm-6">
+          <a href="#something">
+            <img className="img-fluid d-block mx-auto" src="img/logos/envato.jpg" alt=""/>
+          </a>
+        </div>
+        <div className="col-md-3 col-sm-6">
+          <a href="#something">
+            <img className="img-fluid d-block mx-auto" src="img/logos/designmodo.jpg" alt=""/>
+          </a>
+        </div>
+        <div className="col-md-3 col-sm-6">
+          <a href="#something">
+            <img className="img-fluid d-block mx-auto" src="img/logos/themeforest.jpg" alt=""/>
+          </a>
+        </div>
+        <div className="col-md-3 col-sm-6">
+          <a href="#something">
+            <img className="img-fluid d-block mx-auto" src="img/logos/creative-market.jpg" alt=""/>
+          </a>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  
+  <section className="page-section" id="contact">
+    <div className="container">
+      <div className="row">
+        <div className="col-lg-12 text-center">
+          <h2 className="section-heading text-uppercase">Contact Us</h2>
+          <h3 className="section-subheading text-muted">Lorem ipsum dolor sit amet consectetur.</h3>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-lg-12">
+          <form id="contactForm" name="sentMessage" novalidate="novalidate">
+            <div className="row">
+              <div className="col-md-6">
+                <div className="form-group">
+                  <input className="form-control" id="name" type="text" placeholder="Your Name *" required="required" data-validation-required-message="Please enter your name."/>
+                  <p className="help-block text-danger"></p>
+                </div>
+                <div className="form-group">
+                  <input className="form-control" id="email" type="email" placeholder="Your Email *" required="required" data-validation-required-message="Please enter your email address."/>
+                  <p className="help-block text-danger"></p>
+                </div>
+                <div className="form-group">
+                  <input className="form-control" id="phone" type="tel" placeholder="Your Phone *" required="required" data-validation-required-message="Please enter your phone number."/>
+                  <p className="help-block text-danger"></p>
+                </div>
+              </div>
+              <div className="col-md-6">
+                <div className="form-group">
+                  <textarea className="form-control" id="message" placeholder="Your Message *" required="required" data-validation-required-message="Please enter a message."></textarea>
+                  <p className="help-block text-danger"></p>
+                </div>
+              </div>
+              <div className="clearfix"></div>
+              <div className="col-lg-12 text-center">
+                <div id="success"></div>
+                <button id="sendMessageButton" className="btn btn-primary btn-xl text-uppercase" type="submit">Send Message</button>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  
+  <footer className="footer">
+    <div className="container">
+      <div className="row align-items-center">
+        <div className="col-md-4">
+          <span className="copyright">Copyright &copy; Your Website 2019</span>
+        </div>
+        <div className="col-md-4">
+          <ul className="list-inline social-buttons">
+            <li className="list-inline-item">
+              <a href="#something">
+                <i className="fa fa-twitter"></i>
+              </a>
+            </li>
+            <li className="list-inline-item">
+              <a href="#something">
+                <i className="fa fa-facebook-f"></i>
+              </a>
+            </li>
+            <li className="list-inline-item">
+              <a href="#something">
+                <i className="fa fa-linkedin-in"></i>
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div className="col-md-4">
+          <ul className="list-inline quicklinks">
+            <li className="list-inline-item">
+              <a href="#something">Privacy Policy</a>
+            </li>
+            <li className="list-inline-item">
+              <a href="#something">Terms of Use</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </footer>
     </div>
   );
 }
